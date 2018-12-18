38346c441149b74c3491d591e6e4e251040b8f99
chiroll <chiroll@121.132.46.193>
2018-12-18 18:26:26 +0900

    연산자 관련 코드 업데이트

--- src/literal.java
+++ src/literal.java
@@ -2,7 +2,6 @@
 public class literal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		boolean bool = true; // boolean은 논리변수로 true 혹은 false 2가지 리터럴을 지정할 수 있다.
 		System.out.print("boolean:");
 		System.out.println(bool);
@@ -32,5 +31,17 @@ public class literal {
 		System.out.println(d);
 		
 	}
-
+	public static void main2(String[] args) {
+		int i = 50000;
+		long l = i;
+		//  int -> long은 자동으로 형변환이 가능하다. 왜냐하면 int에 지정 가능한 리터럴 값은 long에 지정될 수 있는 리터럴 값보다 클 수 없기 때문이다.
+		
+		long l2 = 5;
+		// 'int i2 = l2;' = 에러코드 출력
+		int i2 =(int) l2;
+		/* 하지만 반대로 long -> int는 자동으로 형변환이 불가능하다. 
+		 * 왜냐하면 long에 지정된 리터럴이 int에도 지정될 수 있는 작은 정수라고 해도 컴파일은 그것을 감지하지 못하기 떄문이다.
+		 * 따라서 이를 강제로 형변환하기 위해서는 변환 값 앞에 '(int)'와 같이 바뀔 변수형을 지정해 주어야한다. (이를 강제 형변환이라 한다)
+		 */
+	}
 }
---
+++ src/operator2.java
@@ -0,0 +1,20 @@
+
+public class operator2 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 10;
+		
+		System.out.println(i == j); // i = j boolean 값 출력
+		System.out.println(i <= j); // i ≤ j boolean 값 출력
+		System.out.println(i != j); // i = j boolean 값 반대로 출력
+		
+		i += 10; // i = i + 10;
+		
+		System.out.println(i);
+		
+		System.out.println(i -= 10);
+		System.out.println(i);
+	}
+
+}
