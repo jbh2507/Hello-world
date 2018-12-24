9ca5062cd1d1e8a16e8f91dcd617ff47f036f54e
chiroll <chiroll@121.132.46.193>
2018-12-24 18:56:44 +0900

    field & method

---
+++ src/classExam.java
@@ -0,0 +1,5 @@
+	// 필드 실습을 위한 class (fieldExam class에서 이용하기 위한 field선언)
+public class classExam {
+	String name; // classExam class에 String type의 'name'이라는 이름을 가진 필드를 생성
+	int number;
+}
---
+++ src/fieldExam.java
@@ -0,0 +1,22 @@
+
+public class fieldExam {
+
+	public static void main(String[] args) {
+		classExam c1 = new classExam();
+		classExam c2 = new classExam();
+		
+		c1.name = "class01";
+		c1.number = 001;
+		
+		c2.name = "class02";
+		c2.number = 002;
+		
+		System.out.println(c1.name);
+		System.out.println(c1.number);
+		
+		System.out.println(c2.name);
+		System.out.println(c2.number);
+
+	}
+
+}
